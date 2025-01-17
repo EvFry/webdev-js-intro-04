6a6881d9115b22104d7e8c3421b132c58af4a290
Evlen Fryling <evlen.fryling@gmail.com>
2025-01-17 15:33:06 -0500

    Task 1 complete

--- js/index.js
+++ js/index.js
@@ -1,3 +1,73 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+/*
+let healthPoints =11;
+
+if (healthPoints >= 10) {
+    console.log("Hero is healthy.");
+} else if (healthPoints < 10 && healthPoints >= 5) {
+    console.log("Hero is slightly damaged.");
+} else {
+    console.log("Hero is close to death.");
+}
+
+//if then statments
+let number = 5;
+if (number > 0) {
+  console.log("The number is positive.");
+}
+let isLoggedIn = true;
+if (isLoggedIn) {
+  console.log("Welcome back!");
+}
+let score = 80;
+if (score >= 70) {
+  console.log("You passed the test!");
+}
+
+//if/else statments
+let number2 = 7;
+if (number % 2 === 0) {
+  console.log("The number is even.");
+} else {
+  console.log("The number is odd.");
+}
+let isAdmin = false;
+if (isAdmin) {
+  console.log("Access granted: Admin privileges.");
+} else {
+  console.log("Access denied: Admin privileges required.");
+}
+let temperature = 30;
+if (temperature <= 32) {
+  console.log("It's freezing outside.");
+} else {
+  console.log("The temperature is above freezing.");
+}
+//if/else if/else statments
+let score = 85;
+if (score >= 90) {
+  console.log("Grade: A");
+} else if (score >= 80) {
+  console.log("Grade: B");
+} else {
+  console.log("Grade: C or lower");
+}
+let hour = 14; // 24-hour format
+if (hour < 12) {
+  console.log("Good morning!");
+} else if (hour < 18) {
+  console.log("Good afternoon!");
+} else {
+  console.log("Good evening!");
+}
+let speed = 75;
+if (speed <= 60) {
+  console.log("You are within the speed limit.");
+} else if (speed <= 80) {
+  console.log("You are speeding. Slow down!");
+} else {
+  console.log("You are dangerously overspeeding!");
+}
+*/
